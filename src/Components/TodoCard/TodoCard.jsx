c0a21bd99d3047a66b83670594c7ace099306121
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-05-20 13:43:25 +0530

    design refactors

--- src/Components/TodoCard/TodoCard.jsx
+++ src/Components/TodoCard/TodoCard.jsx
@@ -27,9 +27,23 @@ const TodoCard = ({ todo, isGridView }) => {
       </div>
       
       <div className={`flex ${isGridView ? 'flex-col sm:flex-row justify-around md:gap-2' : 'flex-row items-center justify-end gap-2'}`}>
-        <EditTodo todo={todo} />
-        <DeleteTodo todoId={todo.id} />
-        {!isGridView && <CompleteTodo todo={todo} />}
+      {!isGridView && <div className='flex flex-col'>
+          <div className='flex justify-end'>
+            <CompleteTodo todo={todo} />
+          </div>
+          <div className='flex gap-2'>
+            <EditTodo todo={todo} />
+            <DeleteTodo todoId={todo.id} />
+          </div>
+        </div>
+      }
+
+      {isGridView&&
+        <div className='flex gap-5 justify-between'>
+          <EditTodo todo={todo} />
+          <DeleteTodo todoId={todo.id} />
+        </div>
+      }
       </div>
     </div>
   );
